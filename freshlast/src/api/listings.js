dd0d96581f5089d9c0a528fe54ddb1be644a4cfa
chrisb588 <brilloschristian0@gmail.com>
2026-02-24 09:21:32 +0800

    added create listing api endpoint on frontend

--- freshlast/src/api/listings.js
+++ freshlast/src/api/listings.js
@@ -1,6 +1,6 @@
-import { supabase } from "../lib/supabaseClient";
+import apiClient from "../lib/apiClient";
 
-async function createListing(
+const createListing = (
   merchantId,
   name,
   price,
@@ -8,32 +8,8 @@ async function createListing(
   unit,
   quantity,
   expirationDetails,
-) {
-  // Check if all types are correct since this is Javascript
-  if (
-    typeof merchantId != Number ||
-    typeof name != String ||
-    typeof price != Number ||
-    !(typeof image == String || image == null) ||
-    typeof unit != String ||
-    typeof quantity != Number ||
-    !(typeof expirationDetails == String || expirationDetails == null)
-  ) {
-    throw "Error when creating a new listing: Input values are invalid.";
-  }
-
-  // Check if there are null values
-  if (
-    merchantId == null ||
-    name == null ||
-    price == null ||
-    unit == null ||
-    quantity == null
-  ) {
-    throw "Error when creating a new listing: All fields must be filled up.";
-  }
-
-  const { error } = supabase.from("listing").insert({
+) =>
+  apiClient.post("/listing", {
     merchant_id: merchantId,
     name: name,
     price: price,
@@ -43,9 +19,4 @@ async function createListing(
     expiration_details: expirationDetails,
   });
 
-  if (error != null) {
-    throw `Error when creating a new listing: ${error}.`;
-  }
-}
-
-export { create };
+export { createListing };
