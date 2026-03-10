9e389e94c778f75dbea8d2ddd836509ac52f877b
awesomeFBK <jasonsalgadomahinay@gmail.com>
2026-03-10 11:02:37 +0800

    More createListing edits

--- freshlast/src/components/ListingForm/ListingForm.jsx
+++ freshlast/src/components/ListingForm/ListingForm.jsx
@@ -8,57 +8,88 @@ export default function ListingForm(){
                 <h4>Create New Listing</h4>
                 <p>Add a new product to your stall.</p>
             </div>
-            <div className="info-container">
-                <p>product photo</p>
+            <div className="info-container-photo">
+                <div className="info-preface">
+                    <h4>Product photo</h4>
+                    <p>Please add a photo for your product</p>
+                </div>
                 <div className="img-container">
                     <img src={addimgicon} className ="addimgicon"/>
                 </div>
             </div>
             <div className="info-container">
-                <h4>product details</h4>
-                <p>Enter details about your products</p>
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="name of product"
-                    className="formInput"
-                    />
-                <input 
-                    type="number" 
-                    name="price"
-                    min="0"
-                    step="0.01" // Allows decimals for price
-                    placeholder="0.00"
-                    />
-                <label htmlFor="unit">Unit</label>
-                <select 
-                    name="unit" 
-                    id="unit"
-                    >
-                    <option value="kg">kg</option>
-                    <option value="lbs">lbs</option>
-                </select>
+                <div className="info-preface">
+                    <h4>product details</h4>
+                    <p>Enter details about your products</p>
+                </div>
+                <div className="descriptionInput">
+                    <label htmlFor="productName">Product Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="productName"
+                        placeholder="e.g. Cabbage, Carrot, etc."
+                        className="formInput"
+                        />
+                    <div className="quantityInput">
+                        <div className="quantityWrapper">
+                            <label htmlFor="quantity">Quantity Available</label>
+                            <br/>
+                            <input 
+                                type="number" 
+                                name="quantity"
+                                id="quantity"
+                                min="0"
+                                step="0.01" // Allows decimals for price
+                                placeholder="0.00"
+                                />
+                        </div>
+                        <div className="quantityWrapper">
+                            <label htmlFor="unit">Unit</label>
+                            <br/>
+                            <select 
+                                name="unit" 
+                                id="unit"
+                                >
+                                <option value="kg">kg</option>
+                                <option value="lbs">lbs</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="info-container">
-                <p>Price</p>
-                <label htmlFor="originalprice">Original Price</label>
-                <input 
-                    type="number" 
-                    name="originalprice"
-                    id="originalprice"
-                    min="0"
-                    step="0.01" // Allows decimals for price
-                    placeholder="0.00"
-                />
-                <label htmlFor="discountedprice">Discounted Price</label>
-                <input 
-                    type="number" 
-                    name="discountedprice"
-                    id="discountedprice"
-                    min="0"
-                    step="0.01" // Allows decimals for price
-                    placeholder="0.00"
-                />
+                <div className="info-preface">
+                    <h4>Pricing</h4>
+                    <p>Enter your original and discounted prices</p>
+                </div>
+                <div className="priceInput">
+                    <div className="pricing">
+                        <label htmlFor="originalprice">Original Price</label>
+                        <br></br>
+                        <input 
+                            type="number"
+
+                            name="originalprice"
+                            id="originalprice"
+                            min="0"
+                            step="0.01" // Allows decimals for price
+                            placeholder="0.00"
+                        />
+                    </div>
+                    <div className="pricing">
+                    <label htmlFor="discountedprice">Discounted Price</label>
+                    <br></br>
+                    <input 
+                        type="number" 
+                        name="discountedprice"
+                        id="discountedprice"
+                        min="0"
+                        step="0.01" // Allows decimals for price
+                        placeholder="0.00"
+                    />
+                    </div>
+                </div>
             </div>
 
         </div>
