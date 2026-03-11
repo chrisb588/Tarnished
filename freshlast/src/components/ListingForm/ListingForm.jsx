303536fb8b132fa28b8ef036fd71b016cc40e10b
awesomeFBK <jasonsalgadomahinay@gmail.com>
2026-03-11 13:55:09 +0800

    transfer from laptop to pc

--- freshlast/src/components/ListingForm/ListingForm.jsx
+++ freshlast/src/components/ListingForm/ListingForm.jsx
@@ -1,7 +1,38 @@
+import { useState } from 'react';
 import './ListingForm.css'
 import addimgicon from "../../assets/testImg.png"
 
 export default function ListingForm(){
+    const [formData, setFormData] = useState({
+    name: "",
+    quantity: 0,
+    unit: "",
+    originalprice: 0,
+    discountedprice: 0,
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const handleStepper = (change) => {
+        setFormData((prev) => {
+            const newQuantity = prev.quantity + change;
+
+            if (newQuantity < 0) return prev;
+
+            return{
+                ...prev,
+                quantity: newQuantity
+            }
+
+        })
+    }
+
     return(
         <div className="create-container">
             <div className="preface">
@@ -12,6 +43,7 @@ export default function ListingForm(){
                 <div className="info-preface">
                     <h4>Product photo</h4>
                     <p>Please add a photo for your product</p>
+                    {/*Do photo logic here soon*/}
                 </div>
                 <div className="img-container">
                     <img src={addimgicon} className ="addimgicon"/>
@@ -19,7 +51,7 @@ export default function ListingForm(){
             </div>
             <div className="info-container">
                 <div className="info-preface">
-                    <h4>product details</h4>
+                    <h4>Product Details</h4>
                     <p>Enter details about your products</p>
                 </div>
                 <div className="descriptionInput">
@@ -30,26 +62,38 @@ export default function ListingForm(){
                         id="productName"
                         placeholder="e.g. Cabbage, Carrot, etc."
                         className="formInput"
+                        value={formData.name}
+                        onChange={handleChange}
                         />
-                    <div className="quantityInput">
+                    <div className="quantityDiv">
                         <div className="quantityWrapper">
                             <label htmlFor="quantity">Quantity Available</label>
                             <br/>
-                            <input 
-                                type="number" 
-                                name="quantity"
-                                id="quantity"
-                                min="0"
-                                step="0.01" // Allows decimals for price
-                                placeholder="0.00"
-                                />
+                            <div className="quantityStepper">
+                                <button 
+                                className="stepper-button"
+                                onClick={() => handleStepper(-1)}>-</button>
+                                <input 
+                                    type="number" 
+                                    name="quantity"
+                                    id="quantity"
+                                    min="0"
+                                    placeholder="0.00"
+                                    value={formData.quantity}
+                                    onChange={handleChange}
+                                    />
+                                <button 
+                                className="stepper-button"
+                                onClick={() => handleStepper(+1)}>+</button>
+                            </div>
                         </div>
-                        <div className="quantityWrapper">
+                        <div className="unitWrapper">
                             <label htmlFor="unit">Unit</label>
-                            <br/>
                             <select 
                                 name="unit" 
                                 id="unit"
+                                value={formData.unit}
+                                onChange={handleChange}
                                 >
                                 <option value="kg">kg</option>
                                 <option value="lbs">lbs</option>
@@ -69,11 +113,11 @@ export default function ListingForm(){
                         <br></br>
                         <input 
                             type="number"
-
+                            value={formData.originalprice}
+                            onChange={handleChange}
                             name="originalprice"
                             id="originalprice"
                             min="0"
-                            step="0.01" // Allows decimals for price
                             placeholder="0.00"
                         />
                     </div>
@@ -83,14 +127,16 @@ export default function ListingForm(){
                     <input 
                         type="number" 
                         name="discountedprice"
+                        value={formData.discountedprice}
+                        onChange={handleChange}
                         id="discountedprice"
                         min="0"
-                        step="0.01" // Allows decimals for price
                         placeholder="0.00"
                     />
                     </div>
                 </div>
             </div>
+
 
         </div>
     )
--- freshlast/src/pages/CreateListing/CreateListing.jsx
+++ freshlast/src/pages/CreateListing/CreateListing.jsx
@@ -4,13 +4,28 @@ import ListingForm from '../../components/ListingForm/ListingForm';
 
 import './CreateListing.css'
 export default function CreateListing(){
+    const [isCreating, setIsCreating] = useState(true);
     return(
         <div className="main-body">
             <Link to="/" className="floating-add-btn">
                     <p>Back to home</p>
                 </Link>
                 
-            <ListingForm></ListingForm>
+            <ListingForm/>
+
+            {isCreating ? (
+                <div className='button-div'>
+                    <button className='create-button'>Create Listing</button>
+                </div>
+            ) :
+            (
+                <>
+                <div className='button-div'>
+                    <button className='save-edit-button'>Save Listing</button>
+                    <button className='delete-button'>Delete Listing</button>
+                </div>
+                </>
+            )} 
 
         </div>
     )
