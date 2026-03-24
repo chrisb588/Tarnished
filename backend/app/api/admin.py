d9b31084dcc63d24a6a0adde355b860e10a97338
chrisb588 <brilloschristian0@gmail.com>
2026-03-24 19:04:27 +0800

    removed response model

--- backend/app/api/admin.py
+++ backend/app/api/admin.py
@@ -5,16 +5,15 @@ from uuid import UUID
 
 import passgen
 from core.supabase import supabase
-from fastapi import APIRouter, status
+from fastapi import APIRouter, HTTPException, status
 from models.admin import CreateMerchantRequestPayloadModel
-from models.core import ResponseModel
 from models.profile import Merchant
 
 router = APIRouter()
 
 
 # Create a merchant account
-@router.post("/create", response_model=ResponseModel, tags=["Admin"])
+@router.post("/create", tags=["Admin"])
 def create_merchant(payload: CreateMerchantRequestPayloadModel):
     try:
         generated_password = passgen.passgen(length=8)
@@ -27,10 +26,10 @@ def create_merchant(payload: CreateMerchantRequestPayloadModel):
             }
         )
     except Exception as e:
-        return ResponseModel(status=status.HTTP_400_BAD_REQUEST, message=str(e))
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
     try:
-        data = (
+        (
             supabase.table("merchant")
             .insert(
                 Merchant(
@@ -47,27 +46,20 @@ def create_merchant(payload: CreateMerchantRequestPayloadModel):
             .execute()
         )
 
-        return ResponseModel(
-            status=status.HTTP_200_OK,
-            payload=json.dumps(
-                {
-                    "uuid": create_response.user.id,
-                    "email": payload.email,
-                    "temp-password": generated_password,
-                }
-            ),
-            message=f"Account {create_response.user.id} with email {payload.email} has been created.\n"
-            + str(data),
-        )
+        return {
+            "uuid": create_response.user.id,
+            "email": payload.email,
+            "temp_password": generated_password,
+        }
 
     except Exception as e:
         # Deletes the user when it is created
         try:
             supabase.auth.admin.delete_user(create_response.user.id)
 
-            return ResponseModel(status=status.HTTP_400_BAD_REQUEST, message=str(e))
+            return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
         except Exception as e:
-            return ResponseModel(
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                message="Error occurred when trying to clean up users table on failed create_merchant call",
+            return HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Error occurred when trying to clean up users table on failed create_merchant call",
             )
--- backend/app/models/core.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Json
-
-
-class ResponseModel(BaseModel):
-    status: int
-    payload: Json | None = None
-    message: str
