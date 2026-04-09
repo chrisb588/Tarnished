241d2433a06d781cd99c0cf9abd4419fb532c421
awesomeFBK <jasonsalgadomahinay@gmail.com>
2026-04-09 15:21:25 +0800

    before vibecode

--- freshlast/src/components/MerchantDisplayAdmin/MerchantDisplayAdmin.jsx
+++ freshlast/src/components/MerchantDisplayAdmin/MerchantDisplayAdmin.jsx
@@ -1,14 +1,15 @@
 import './MerchantDisplayAdmin.css'
+import {FaRegTrashAlt, FaPencilAlt} from 'react-icons/fa'
 
-export default function ListingItem({ listing, showEdit = false }) {
+export default function MerchantDisplayAdmin({ merchant }) {
   return (
     <div className="merchant-container">
         <div className='merchant-name'>
-          <p>Test Name</p>
+          <p>{merchant.name}</p>
         </div>
         <div className='button-container'>
-          <button className='edit-button'>E</button>
-          <button className='delete-button'>D</button>
+          <button className='edit-button'><FaPencilAlt/></button>
+          <button className='delete-button'><FaRegTrashAlt/></button>
         </div>
     </div>
   );
