a2f8b7e495c7ec731e927c2e3d01b44d1a6c07f9
awesomeFBK <jasonsalgadomahinay@gmail.com>
2026-04-09 15:30:42 +0800

    UI for admin dashboard complete

--- freshlast/src/pages/AdminDashboard/AdminDashboard.jsx
+++ freshlast/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,9 +1,15 @@
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import './AdminDashboard.css'
+import { useState } from 'react';
 import MerchantDisplayAdmin from '../../components/MerchantDisplayAdmin/MerchantDisplayAdmin'
 
 export default function AdminDashboard(){
 const navigate = useNavigate();
+//dummy data
+const [merchants, setMerchants] = useState([
+    { id: 1, name: "Pizza Palace" },
+    { id: 2, name: "Tech To Go" }
+]);
 
     return (
         <div className='container'>
@@ -15,10 +21,9 @@ const navigate = useNavigate();
                 <h3>Users</h3>
                 <div
                 className='user-list'>
-                    <MerchantDisplayAdmin/>
-                    <MerchantDisplayAdmin/>
-                    <MerchantDisplayAdmin/>
-                    <MerchantDisplayAdmin/>
+                    {merchants.map((merchant) => (
+                        <MerchantDisplayAdmin key={merchant.id} merchant={merchant} />
+                    ))}
                 </div>
                 <button
                 className='add-vendor-btn'
