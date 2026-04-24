096a469b69e3d55e03dcd710991746d4eb776c30
chrisb588 <brilloschristian0@gmail.com>
2026-04-24 17:27:43 +0800

    test: backend: added test cases for admin endpoint guards

--- backend/tests/api/test_admin.py
+++ backend/tests/api/test_admin.py
@@ -1,5 +1,99 @@
 import io
 import json
+import os
+from datetime import datetime, timedelta
+
+import jwt
+from dotenv import load_dotenv
+
+load_dotenv(
+    dotenv_path=os.path.join(os.path.dirname(__file__), ".env.test"), override=True
+)
+
+
+def test_login_admin_success(client):
+    response = client.post(
+        "/api/admin/auth/login",
+        json={
+            "username": os.getenv("ADMIN_USERNAME", ""),
+            "password": os.getenv("ADMIN_PASSWORD", ""),
+        },
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert "access_token" in data
+
+
+def test_login_admin_invalid_credentials(client):
+    response = client.post(
+        "/api/admin/auth/login",
+        json={
+            "username": "not the correct username",
+            "password": "not the correct password",
+        },
+    )
+    assert response.status_code == 401
+
+
+def test_login_admin_incomplete_payload(client):
+    # No username
+    response = client.post(
+        "/api/admin/auth/login",
+        json={
+            "password": os.getenv("ADMIN_PASSWORD", ""),
+        },
+    )
+    assert response.status_code == 422
+
+    # No password
+    response = client.post(
+        "/api/admin/auth/login",
+        json={
+            "username": os.getenv("ADMIN_USERNAME", ""),
+        },
+    )
+    assert response.status_code == 422
+
+
+def test_is_admin_authenticated_success(client):
+    response = client.get(
+        "/api/admin/auth/verify",
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data["valid"]
+
+
+def test_is_admin_authenticated_invalid_token(client):
+    response = client.get(
+        "/api/admin/auth/verify",
+        headers={"Authorization": "Bearer not-the-correct-token"},
+    )
+    assert response.status_code == 401
+
+
+def test_is_admin_authenticated_invalid_header(client):
+    response = client.get(
+        "/api/admin/auth/verify",
+        headers={"Some-Header": f"Bearer {generate_test_admin_token()}"},
+    )
+    assert response.status_code == 401
+
+
+def test_is_admin_authenticated_no_bearer_prefix(client):
+    response = client.get(
+        "/api/admin/auth/verify",
+        headers={"Authorization": generate_test_admin_token()},
+    )
+    assert response.status_code == 401
+
+
+def test_is_admin_authenticated_no_token(client):
+    response = client.get(
+        "/api/admin/auth/verify",
+    )
+    assert response.status_code == 401
 
 
 def test_create_merchant_success(client):
@@ -24,6 +118,7 @@ def test_create_merchant_success(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
 
     assert response.status_code == 200
@@ -56,6 +151,7 @@ def test_create_merchant_unprocessable_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -81,6 +177,7 @@ def test_create_merchant_unprocessable_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -106,6 +203,7 @@ def test_create_merchant_unprocessable_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -131,6 +229,7 @@ def test_create_merchant_unprocessable_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -156,6 +255,7 @@ def test_create_merchant_unprocessable_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -181,6 +281,7 @@ def test_create_merchant_unprocessable_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -206,6 +307,7 @@ def test_create_merchant_unprocessable_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -225,6 +327,7 @@ def test_create_merchant_unprocessable_payload(client):
             "category": json.dumps(["vegetable"]),
         },
         files={"location_photo": "invalid-photo"},
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -250,6 +353,7 @@ def test_create_merchant_unprocessable_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -270,13 +374,14 @@ def test_create_merchant_user_already_exists(client):
     files = {
         "location_photo": ("test.jpg", io.BytesIO(b"fake-image-bytes"), "image/jpeg")
     }
+    headers = {"Authorization": f"Bearer {generate_test_admin_token()}"}
 
-    client.post("/api/admin/create", data=data, files=files)
+    client.post("/api/admin/create", data=data, files=files, headers=headers)
 
     files = {
         "location_photo": ("test.jpg", io.BytesIO(b"fake-image-bytes"), "image/jpeg")
     }
-    response = client.post("/api/admin/create", data=data, files=files)
+    response = client.post("/api/admin/create", data=data, files=files, headers=headers)
 
     assert response.status_code == 400
 
@@ -303,6 +408,7 @@ def test_create_merchant_incomplete_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -327,6 +433,7 @@ def test_create_merchant_incomplete_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -351,6 +458,7 @@ def test_create_merchant_incomplete_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -375,6 +483,7 @@ def test_create_merchant_incomplete_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -399,6 +508,7 @@ def test_create_merchant_incomplete_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -423,6 +533,7 @@ def test_create_merchant_incomplete_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -447,6 +558,7 @@ def test_create_merchant_incomplete_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -471,6 +583,7 @@ def test_create_merchant_incomplete_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -488,6 +601,7 @@ def test_create_merchant_incomplete_payload(client):
             "location": "Cebu City",
             "category": json.dumps(["vegetable"]),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -512,6 +626,7 @@ def test_create_merchant_incomplete_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
 
@@ -536,8 +651,89 @@ def test_create_merchant_incomplete_payload(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 422
+
+
+def test_create_merchant_no_headers(client):
+    response = client.post(
+        "/api/admin/create",
+        data={
+            "email": "email@example.com",
+            "name": "Test Merchant",
+            "phone_number": "+639123456789",
+            "latitude": 10.3157,
+            "longitude": 123.8854,
+            "start_operating_time": "08:00:00",
+            "end_operating_time": "17:00:00",
+            "operating_days": json.dumps(["Mon", "Wed", "Fri"]),
+            "location": "Cebu City",
+            "category": json.dumps(["vegetable"]),
+        },
+        files={
+            "location_photo": (
+                "test.jpg",
+                io.BytesIO(b"fake-image-bytes"),
+                "image/jpeg",
+            ),
+        },
+    )
+    assert response.status_code == 401
+
+
+def test_create_merchant_invalid_headers(client):
+    # Invalid authorization header label
+    response = client.post(
+        "/api/admin/create",
+        data={
+            "email": "email@example.com",
+            "name": "Test Merchant",
+            "phone_number": "+639123456789",
+            "latitude": 10.3157,
+            "longitude": 123.8854,
+            "start_operating_time": "08:00:00",
+            "end_operating_time": "17:00:00",
+            "operating_days": json.dumps(["Mon", "Wed", "Fri"]),
+            "location": "Cebu City",
+            "category": json.dumps(["vegetable"]),
+        },
+        files={
+            "location_photo": (
+                "test.jpg",
+                io.BytesIO(b"fake-image-bytes"),
+                "image/jpeg",
+            ),
+        },
+        headers={"Some-Header": f"Bearer {generate_test_admin_token()}"},
+    )
+    assert response.status_code == 401
+
+    # No bearer prefix
+    response = client.post(
+        "/api/admin/create",
+        data={
+            "email": "email@example.com",
+            "name": "Test Merchant",
+            "phone_number": "+639123456789",
+            "latitude": 10.3157,
+            "longitude": 123.8854,
+            "start_operating_time": "08:00:00",
+            "end_operating_time": "17:00:00",
+            "operating_days": json.dumps(["Mon", "Wed", "Fri"]),
+            "location": "Cebu City",
+            "category": json.dumps(["vegetable"]),
+        },
+        files={
+            "location_photo": (
+                "test.jpg",
+                io.BytesIO(b"fake-image-bytes"),
+                "image/jpeg",
+            ),
+        },
+        headers={"Authorization": generate_test_admin_token()},
+    )
+    assert response.status_code == 401
 
 
 def test_delete_merchant_success(client):
@@ -563,24 +759,110 @@ def test_delete_merchant_success(client):
                 "image/jpeg",
             ),
         },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
     )
     assert response.status_code == 200
     user = response.json()
-    print(user)
 
-    response = client.delete(f"/api/admin/delete/{user['uuid']}")
+    response = client.delete(
+        f"/api/admin/delete/{user['uuid']}",
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
+    )
     assert response.status_code == 200
 
 
 def test_delete_merchant_when_not_found(client):
     from uuid import uuid4
 
-    response = client.delete(f"/api/admin/delete/{uuid4()}")
+    response = client.delete(
+        f"/api/admin/delete/{uuid4()}",
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
+    )
     assert response.status_code == 400
 
 
+def test_delete_merchant_no_headers(client):
+    # Create the user first
+    response = client.post(
+        "/api/admin/create",
+        data={
+            "email": "email@example.com",
+            "name": "Test Merchant",
+            "phone_number": "+639123456789",
+            "latitude": 10.3157,
+            "longitude": 123.8854,
+            "start_operating_time": "08:00:00",
+            "end_operating_time": "17:00:00",
+            "operating_days": json.dumps(["Mon", "Wed", "Fri"]),
+            "location": "Cebu City",
+            "category": json.dumps(["vegetable"]),
+        },
+        files={
+            "location_photo": (
+                "test.jpg",
+                io.BytesIO(b"fake-image-bytes"),
+                "image/jpeg",
+            ),
+        },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
+    )
+    assert response.status_code == 200
+    user = response.json()
+
+    response = client.delete(
+        f"/api/admin/delete/{user['uuid']}",
+    )
+    assert response.status_code == 401
+
+
+def test_delete_merchant_invalid_headers(client):
+    # Create the user first
+    response = client.post(
+        "/api/admin/create",
+        data={
+            "email": "email@example.com",
+            "name": "Test Merchant",
+            "phone_number": "+639123456789",
+            "latitude": 10.3157,
+            "longitude": 123.8854,
+            "start_operating_time": "08:00:00",
+            "end_operating_time": "17:00:00",
+            "operating_days": json.dumps(["Mon", "Wed", "Fri"]),
+            "location": "Cebu City",
+            "category": json.dumps(["vegetable"]),
+        },
+        files={
+            "location_photo": (
+                "test.jpg",
+                io.BytesIO(b"fake-image-bytes"),
+                "image/jpeg",
+            ),
+        },
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
+    )
+    assert response.status_code == 200
+    user = response.json()
+
+    # Invalid authorization header label
+    response = client.delete(
+        f"/api/admin/delete/{user['uuid']}",
+        headers={"Some-Header": f"Bearer {generate_test_admin_token()}"},
+    )
+    assert response.status_code == 401
+
+    # No bearer prefix
+    response = client.delete(
+        f"/api/admin/delete/{user['uuid']}",
+        headers={"Authorization": generate_test_admin_token()},
+    )
+    assert response.status_code == 401
+
+
 def test_get_all_merchants_success(client):
-    response = client.get("/api/admin/merchants")
+    response = client.get(
+        "/api/admin/merchants",
+        headers={"Authorization": f"Bearer {generate_test_admin_token()}"},
+    )
     assert response.status_code == 200
     data = response.json()
     assert len(data) == 1
@@ -599,3 +881,34 @@ def test_get_all_merchants_success(client):
         == "http://127.0.0.1:54321/storage/v1/object/public/media/11111111-1111-1111-1111-111111111111/profile/22222222-2222-2222-2222-222222222222"
     )
     assert data["category"] == ["vegetable"]
+
+
+def test_get_all_merchants_no_headers(client):
+    response = client.get(
+        "/api/admin/merchants",
+    )
+    assert response.status_code == 401
+
+
+def test_get_all_merchants_invalid_headers(client):
+    # Invalid authorization header label
+    response = client.get(
+        "/api/admin/merchants",
+        headers={"Some-Header": f"Bearer {generate_test_admin_token()}"},
+    )
+    assert response.status_code == 401
+
+    # No bearer prefix
+    response = client.get(
+        "/api/admin/merchants",
+        headers={"Authorization": generate_test_admin_token()},
+    )
+    assert response.status_code == 401
+
+
+def generate_test_admin_token():
+    return jwt.encode(
+        {"sub": "admin", "exp": datetime.now() + timedelta(hours=1)},
+        os.getenv("ADMIN_JWT_SECRET", ""),
+        algorithm="HS256",
+    )
