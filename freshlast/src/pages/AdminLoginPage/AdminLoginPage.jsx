5e48cf519213b1f25e2a69284d6416a5ee08cdfb
chrisb588 <brilloschristian0@gmail.com>
2026-05-04 08:35:04 +0800

    fix(admin-auth): fixed button not working in admin auth form

--- freshlast/src/pages/AdminLoginPage/AdminLoginPage.jsx
+++ freshlast/src/pages/AdminLoginPage/AdminLoginPage.jsx
@@ -1,62 +1,62 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import './AdminLoginPage.css'
+import "./AdminLoginPage.css";
+import { adminLogin } from "../../api/admin";
 
+export default function AdminLoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
 
-export default function AdminLoginPage(){
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate()
+    try {
+      await adminLogin(username, password);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setLoading(true);
+      navigate("/");
+      console.log("Admin is authenticated");
+    } catch (error) {
+      alert(error);
+      console.log(error);
+    }
 
-        if (!supabase) {
-        alert("Supabase not configured. Please check environment variables.");
-        setLoading(false);
-        return;
-        }
+    setLoading(false);
+  };
 
-        const { error } = await supabase.auth.signInWithPassword({ email, password });
-
-        if (error) {
-        alert(error.message);
-        } else {
-        onSuccess?.();
-        }
-        setLoading(false);
-    };
-
-    return (
-        <div className="admin-login-form-container">
-            <button className="adminlogin-backbtn" onClick={() => navigate(-1)}>
-                ← Back
-            </button>
-            <form className="admin-login-form" onSubmit={handleSubmit}>
-                <h1 className='admin-login-label'>Sign in as Admin</h1>
-                <p>email address</p>
-                <input
-                type="email"
-                placeholder="Your email"
-                value={email}
-                required
-                onChange={(e) => setEmail(e.target.value)}
-                />
-                <p>password</p>
-                <input
-                type="password"
-                placeholder="Your password"
-                value={password}
-                required
-                onChange={(e) => setPassword(e.target.value)}
-                />            
-                <button disabled={loading} className="admin-login-button">
-                    {loading ? "Processing..." : "Log In"}
-                </button>
-            </form>
-        </div>
-    )
+  return (
+    <div className="admin-login-form-container">
+      <button className="adminlogin-backbtn" onClick={() => navigate(-1)}>
+        ← Back
+      </button>
+      <form className="admin-login-form" onSubmit={handleSubmit}>
+        <h1 className="admin-login-label">Sign in as Admin</h1>
+        <p>username</p>
+        <input
+          type="text"
+          placeholder="Your username"
+          value={username}
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <p>password</p>
+        <input
+          type="password"
+          placeholder="Your password"
+          value={password}
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          disabled={loading}
+          onClick={handleSubmit}
+          className="admin-login-button"
+        >
+          {loading ? "Processing..." : "Log In"}
+        </button>
+      </form>
+    </div>
+  );
 }
